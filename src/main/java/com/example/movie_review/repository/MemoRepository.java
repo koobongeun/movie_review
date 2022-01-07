ce4e78c30541204354a5026c235fb4afd2759874
koobongeun <gbg7524@naver.com>
2022-01-08 02:35:32 +0900

    Part 1 - 스프링 부트 도전하기 / Chapter 02 Maria DB와 Spring Data JPA

---
+++ src/main/java/com/example/movie_review/repository/MemoRepository.java
@@ -0,0 +1,17 @@
+package com.example.movie_review.repository;
+
+import com.example.movie_review.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo,Long> {
+
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    void deleteMemoByMnoLessThan(Long num);
+}
