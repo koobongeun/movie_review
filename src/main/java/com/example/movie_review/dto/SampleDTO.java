5bc2927f186da283535ad936a7850b8cd2bbe87a
koobongeun <gbg7524@naver.com>
2022-01-19 23:43:41 +0900

    Part 1 - 스프링 부트 도전하기 / Chapter 03 스피링 MVC 와 Thymeleaf

---
+++ src/main/java/com/example/movie_review/dto/SampleDTO.java
@@ -0,0 +1,21 @@
+package com.example.movie_review.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+
+
+}
