99647cd9914d02173182f71431d68c9b7f3641b7
koobongeun <gbg7524@naver.com>
2022-01-19 23:43:06 +0900

    Part 1 - 스프링 부트 도전하기 / Chapter 03 스피링 MVC 와 Thymeleaf

--- src/main/java/com/example/movie_review/controller/SampleController.java
+++ src/main/java/com/example/movie_review/controller/SampleController.java
@@ -1,13 +1,40 @@
 package com.example.movie_review.controller;
 
+import com.example.movie_review.dto.SampleDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController // 별도의 화면 없이 데이터를 전송
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
 public class SampleController {
 
-    @GetMapping("/hello")
-    public String[] hello(){
-        return new String[]{"hello", "world"};
+    @GetMapping("/ex1")
+    public void ex1(){
+
+        log.info("ex1.................");
+    }
+
+    @GetMapping({"/ex2"}) // {} 로 처리하면 하나 이상의 URL을 지정할 수 있음.
+    public void exModel(Model model){
+        List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i ->{
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First.."+i)
+                    .last("Last.."+i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
     }
 }
